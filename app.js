81722025bc0bfa3e522794b8579f9d0ad0ad6174
Mitali Mittal <mits254@gmail.com>
2018-07-25 23:17:16 -0500

    part 1

---
+++ app.js
@@ -0,0 +1,15 @@
+// Write a function that returns each string in this array in all uppercase letters.
+let l = ["a","b","c","d","e"];
+for(let i=0; i<l.length; i++) {
+console.log(l[i].toUpperCase());
+}
+
+// Write a function that accepts an array of objects and logs the color attribute of each.
+let list = [
+  {width: 20, color: 'red'},
+  {width: 50, color: 'blue'},
+  {width: 10, color: 'purple'}
+];
+for (let i=0; i < list.length; i++) {
+    console.log(list[i].color);
+}
